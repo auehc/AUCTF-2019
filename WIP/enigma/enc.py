a580125d99c44a154fa5be253e38e268c6f7929f
Jordan Sosnowski <jjs0029@auburn.edu>
2019-03-28 15:29:59 -0500

    Worked on Enigma

--- WIP/enigma/enc.py
+++ WIP/enigma/enc.py
@@ -1,17 +1,25 @@
 from enigma.machine import EnigmaMachine
 
 machine = EnigmaMachine.from_key_sheet(
-       rotors='V II I',
+       rotors='II V I',
        reflector='B',
-       ring_settings='17 18 26',
-       plugboard_settings='UN HT WQ RS DF VI GP XZ MY AK')
+       ring_settings='07 19 02',
+       plugboard_settings='HR NC IU DM TW GV FB ZL EQ OX')
 
-c = 'YRS'
-machine.set_display(c)
-user_input = input("Please enter string to be encoded: ")
+init_rot_pos = 'EHC'
+machine.set_display(init_rot_pos)
+enc_key = machine.process_text('CTF')
+print("Initial Rotor Positions: %s" % init_rot_pos)
+print("Three letter message key: %s" % enc_key)
+
+secret_key = 'CTF'
+machine.set_display(secret_key)
+print("Secret Key: %s" % secret_key)
+
+#user_input = input("Please enter string to be encoded: ")
+#user_input ='AGLARRUZRQGCLHVATCAKXHBLIPFWMRCGHNCYCVUNCSJWAPSNUKBRODCFVBYHKAQ'
+user_input = 'Riddle me this what is always on its way here but never arrives'
 enc = machine.process_text(user_input)
-machine.set_display(c)
-dec = machine.process_text(enc)
 
+print("Original Message: %s" % user_input)
 print("Encoded text: %s" % enc)
-print("Decoded text: %s" % dec.lower().replace('x', ' '))
---
+++ WIP/enigma/writup.md
@@ -0,0 +1,35 @@
+U52 12 C 2300 = 63 = EHC XLW = 
+LXZAL AGLAR RUZRQ GCLHV ATCAK XHBLI PFWMR CGHNC YCVUN CSJWA PSNUK BRODC FVBYH KAQ =
+
+U52: station being transmitted from
+C: station being transmitted to
+12: day 12
+2300: 11 PM
+63: message is 63 characters
+
+LXZAL: kenngruppen, or indicator
+    padded with two random letters, users will have to look at the key sheet
+
+    Indicator could be LXZ / XZA / ZAL 
+
+Now we can get the values from the key sheet for the rotors, reflector, ring settings, and plugboard settings
+
+``` machine = EnigmaMachine.from_key_sheet(
+    rotors='II V I',
+    reflector='B',
+    ring_settings='07 19 02',
+    plugboard_settings='HR NC IU DM TW GV FB ZL EQ OX')
+```
+
+
+Next the receiving operator must decrypt the message key
+``` machine.set_display('EHC')
+    msg_key = machine.process_text('XLW')
+```
+
+This reveals key used to encrypt the message, which is `CTF`
+Now we can decrypte the original message
+``` machine.set_display(msg_key) # CTF
+    plaintext = machine.process_text('AGLARRUZRQGCLHVATCAKXHBLIPFWMRCGHNCYCVUNCSJWAPSNUKBRODCFVBYHKAQ')
+    print(plaintext)
+```
