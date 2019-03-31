744116c2f79581e888d8ef25fe6acf4b29d16b41
Jordan Sosnowski <jjs0029@auburn.edu>
2019-03-31 18:10:47 -0500

    Updated readme and edited keygen, finished rev1

--- reversing/hard_code_string/simple.c
+++ reversing/hard_code_string/simple.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdlib.h>
 
-
-int main(int argc, char** argv)                                                                  
+int main(int argc, char **argv)
 {
     static char wait[] = "Wait this isn\'t supposed to be printed..";
     static char flag[] = "aubie{w3lC0m3_T0_R3v3rS1nG}";
 
-    char test[] = "blah blah blah blah jl lol lalm oaj test";
-    int c = 0;
-    int a = 2400;
-    int b = 233;
-
-    for (int i=0; i<25; i++){
-        c += i;
-    }
-
-    int d = a * b;
-
-    return c;
+    printf("Well I supposed you want the flag right?\n");
+    printf("Well here you go kiddo: \n");
 }
--- reversing/keygen/keygen.c
+++ reversing/keygen/keygen.c
@@ -3,49 +3,48 @@
 #include <string.h>
 #include <stdlib.h>
 
-char * gen_key(const char * key)
+char *gen_key(const char *key)
 {
-    char * result = (char *) malloc(64);
-    
+    char *result = (char *)malloc(64);
+
     int length = strlen(key);
     unsigned char seed = 0x48;
-    for(int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
-        result[i] = 44+(seed*key[i] + seed*10 + 2)%75;
+        result[i] = 48 + (seed * key[i] + seed / 2) % 10;
         seed = result[i];
     }
-    return result; 
+    return result;
 }
 
-_Bool verify_key(char * key)
+_Bool verify_key(char *key)
 {
-    if(strlen(key) < 8 || strlen(key) > 32)
+    if (strlen(key) < 8 || strlen(key) > 32)
     {
         return false;
     }
-    char * result = gen_key(key);
-    // random password: acdefjej2039
-    char * compare = "dG8pAVC]LhZLB";
-    printf("%s", result);
+    char *result = gen_key(key);
+    // random password: 02501799219856
+    char *compare = "255067256325588";
     return !strcmp(compare, result);
 }
 
-int main()                                                                  
+int main()
 {
-    setvbuf(stdout,0, _IONBF,0);
+    setvbuf(stdout, 0, _IONBF, 0);
     printf("\nPlease Enter a product key to continue: \n");
     char pkey[65];
-    fgets((char*)pkey, 65, stdin);
-    if (verify_key(pkey))                                                   
+    fgets((char *)pkey, 65, stdin);
+    if (verify_key(pkey))
     {
-        FILE* infile = fopen("flag.txt", "r");
-        if (infile == NULL)                                                 
+        FILE *infile = fopen("flag.txt", "r");
+        if (infile == NULL)
         {
             printf("Great Work! Too bad the flag is only on the server!\n");
-            return 0;                                                       
+            return 0;
         }
         char output[100];
         fgets(output, 100, infile);
-        printf("%s", output);                                                     
+        printf("%s", output);
     }
 }
