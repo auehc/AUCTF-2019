1dbe55ca82f8c486c2702465f39e19cd1e81c20a
Jordan Sosnowski <jjs0029@auburn.edu>
2019-04-06 11:08:32 -0500

    Update game.py

--- WIP/underflow/game.py
+++ WIP/underflow/game.py
@@ -1,5 +1,6 @@
 from os import system, name
 from random import randint
+from copy import copy
 
 
 def clear():
@@ -35,7 +36,6 @@ class Item:
 
     def __str__(self):
         return self.name.ljust(15) + " " + str(self.value).ljust(10) + " " + str(self.instock) + "x"
-
 
 class Shop():
     def __init__(self, filename):
@@ -74,7 +74,7 @@ class Shop():
     def shop_options(self):
         shop_op = [
             "1. Buy Item",
-            "2. Inspect Item",
+            "2. Check Wallet",
             "3. Leave Shop"]
         inv = show_inventory(self.inventory)
         choices = Options(shop_op, inv)
@@ -107,25 +107,25 @@ class Shop():
                               str(choice.value) + ". How much are you willing to pay?")
                         try:
                             bargin = input()
-                            bargin = int(bargin)
+                            bargin = abs(int(bargin))
                         except ValueError:
                             print("Please only provide numbers")
                             continue
 
-                        if int(bargin) > user.wallet:
+                        if bargin > user.wallet:
                             print(
                                 "What do you think I am stupid? You don't have enough cash for that!")
                             print("Let's try this again...", end=' ')
-                        elif abs(bargin) < choice.value:
+                        elif bargin < choice.value:
                             print("You're gonna need more money than that!")
                             print("Let's try this again...", end=' ')
                         else:
                             print("Here you go!")
                             self.wallet += bargin
-                            choice.instock -= 1
-                            user.inventory.append(choice)   ## TODO need to make copy of choice, right now its using the same choice from 
-                            not_done = False                ## shops inventory therefore when you subtract instock from it it takes it away
-                    else:                                   ## from the user
+                            user.buy(bargin, choice)   
+                            choice.instock -= 1 
+                            not_done = False            
+                    else:                                   
                         not_done = False
             else:
                 print("Thats sold out! Try something else...")
@@ -139,25 +139,39 @@ class User():
         self.wallet = 250
         self.inventory = []
         self.alive = True
+        self.locations = {
+            'desert':0,
+            'oasis':0,
+            'town':0,
+            'shop':0,
+            'tavern':0
+        }
 
     def buy(self, price, item):
+        co_item = copy(item)
         self.wallet -= price
-        self.inventory.append(item)
-
+        self.inventory.append(co_item) 
+                                      
     def inspect_inventory(self):
         if len(self.inventory) == 0:
             print('You open your bag but its completely empty...')
         else:
-            item = self.get_item()
-            inv_msg = "What would you like to do with " + item.name + "?"
-            inv_choices = ['1. Check Value', '2. Inspect', '3. Toss']
-            inv_options = Options(inv_choices, inv_msg)
-            option = inv_options.check_input()
-            if option == '1':
-                print(item.value)
-            elif option == '2':
-                print(item.description)
-                print("Oh! %s" % item.hidden)
+            done = False
+            while not done:
+                item = self.get_item()
+                inv_msg = "What would you like to do with " + item.name + "?"
+                inv_choices = ['1. Check Value', '2. Inspect', '3. Toss', '4. Leave Inventory']
+                inv_options = Options(inv_choices, inv_msg)
+                option = inv_options.check_input()
+                if option == '1':
+                    print(item.value)
+                elif option == '2':
+                    print(item.description)
+                    print("Oh! %s" % item.hidden)
+                elif option == '3':
+                    todo() # TODO
+                elif option == '4':
+                    done = True
 
     def get_item(self):
         item_msg = 'You open your bag and your items jostle around. What would you like to inspect?\n'
@@ -168,11 +182,28 @@ class User():
         if option < len(self.inventory):
             return self.inventory[option]
     
-
+    def check_wallet(self):
+        print("You have %d credits" % self.wallet)
     
 
- 
+class Bank:
+    def __init__(self):
+        self.user_account = 500
 
+    def deposit(self, value, player):
+        value = int(value)
+        if value <= player.wallet:
+            self.user_account += value
+            player.wallet -= value
+
+    def withdraw(self, value, player):
+        value = int(value)
+        if value <= self.user_account:
+            player.wallet += value
+            self.user_account -= value
+
+    def check_balance(self):
+        print("You currently have: %d" % self.user_account)
 
 class Options():
     def __init__(self, choices_in=None, message=''):
@@ -229,40 +260,103 @@ def shop_enter(player):
             else:
                 shop.sell_item(buy_choice, player)
         elif user_input == '2':
-            print("TODO")
+           player.check_wallet()
         elif user_input == '3':
             print("Goodbye!")
             done = True
 
+def todo():
+    print("I haven't programmed that path yet...")
 
-def desert(player):
-    desert_msg = "You see what looks to be a small town in the distance to the south and a sparkling oaisis to the north. Where do you want to go?"
-    desert_choices = ['1. Town', '2. Oasis']
+def bank_loc():
+    global bank
+    global player
+    done = False
+    bank_msg = "Welcome to the bank! What would you like to do?"
+    bank_choices = [
+        "1. Withdraw",
+        "2. Deposit",
+        "3. Check balance",
+        "4. Leave Bank",
+        "5. Inspect Inventory",
+        "6. Check Wallet"
+    ]
+    bank_options = Options(bank_choices, bank_msg)
+    while(not done):
+        user_input = bank_options.check_input()
+        if user_input == "1":
+            withdraw_msg = "How much would you like to withdraw?"
+            withdraw_options = Options(message=withdraw_msg)
+            withdraw = withdraw_options.check_input()
+            bank.withdraw(withdraw, player)
+        elif user_input == "2":
+            deposit_msg = "How much would you like to deposit?"
+            deposit_options = Options(message=deposit_msg)
+            deposit = deposit_options.check_input()
+            bank.deposit(deposit, player)
+        elif user_input == "3":
+            bank.check_balance()
+        elif user_input == "4":
+            done = True
+            print("Goodbye!")
+        elif user_input == "5":
+            player.inspect_inventory()
+        elif user_input == "6":
+            player.check_wallet()
+def desert():
+    global player
+    if player.locations['desert'] == 0:
+        desert_msg = "You see what looks to be a small town in the distance to the south and a sparkling oaisis to the north. Where do you want to go?"
+        player.locations['desert'] += 1
+    else:
+        desert_msg = 'You\'ve made your way back to the desert. Where would you like to head?'
+    desert_choices = ['1. Town', '2. Oasis', '3. Inspect Inventory', '4. Check Wallet']
     desert_options = Options(desert_choices, desert_msg)
     desert_choice = desert_options.check_input()
     if desert_choice == '1':
-        town(player)
+        town()
     elif desert_choice == '2':
-        None # TODO
-def town(player):
+        todo() # TODO
+    elif desert_choice == '3':
+        player.inspect_inventory()
+    elif desert_choice == '4':
+        player.check_wallet()
+        
+def town():
+    global player 
     while player.alive:
-        town_choices = ['1. Shop','2. To the Desert','3. Tavern', '4. Inspect Inventory']
-        town_msg = 'You stumble upon a small town where would you like to head?'
+        town_choices = ['1. Shop','2. To the Desert','3. Bank', '4. Inspect Inventory', '5. Check Wallet']
+        if player.locations['town'] == 0:
+            town_msg = 'You stumble upon a small town where would you like to head?'
+            player.locations['town'] += 1
+        else:
+            town_msg = 'You are in the town. Where would you like to head?'
         locations = Options(town_choices, town_msg)
         user_location = locations.check_input()
         if user_location == '1':
             shop_enter(player)
         elif user_location == '2':
-            None # TODO
+            desert()
         elif user_location == '3':
-            desert(player)
+            bank_loc() # TODO
         elif user_location == '4':
             player.inspect_inventory()
-
+        elif user_location == '5':
+            player.check_wallet()
 
 def main():
-    player = User()
+    global player
+    global bank
+    print("Welcome to the game!")
+    print("....")
+    print("....")
+    print("You awaken in a desert...")
     while player.alive:
-        desert(player)
+        desert()
+
+
+
+player = User()
+bank = Bank()
 
 main()
